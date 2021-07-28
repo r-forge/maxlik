f71e3bcdaabe805fa20b608bffd47272a13249d0
otoomet (Ott Toomet) <otoomet@gmail.com>
2021-07-28 18:42:03 +0000

    removed tinytest branch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/maxlik@1743 af626d81-0c90-417d-8daa-7afe02cc634b

--- branches/tidy-glance/R/storedParameters.R
+++
@@ -1,8 +0,0 @@
-## Return the stored parameters in a 'maxim' object
-
-storedParameters <- function(x, ...)
-    ## stored parameter values at each epoch/iteration
-    UseMethod("storedParameters")
-
-storedParameters.maxim <- function(x, ...)
-    x$parameterStore
